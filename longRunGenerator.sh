dfe0dc300d4f0802dc3fb56e3fbba7f5f0d86212
Clément Robert <clement.robert@protonmail.com>
2016-10-19 10:20:29 +0200

    clarified longRunGenerator.sh and replaced my username by crobert

--- longRunGenerator.sh
+++ longRunGenerator.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+
+#functions and input parameters naming
+#--------------------------------------------------
+
 inputfile=$1
 
 function parser { 
 cat $1 | grep -i $2 | tr -s ' ' | cut -d ' ' -f 2
 }
 
-#read the input file displaying all necessary information
 
+#parsing
+#--------------------------------------------------
+
+#read the input file displaying all necessary information
 simdir=$(parser $inputfile simdir)
 configdir=$simdir/$(parser $inputfile configdir)
 jobsdir=$simdir/$(parser $inputfile jobsdir)
 seedjob=$jobsdir/$(parser $inputfile seedjob)
+
 configfile=$configdir/$(parser $inputfile configfile)
 oph=$(parser $inputfile oph)
 
@@ -19,19 +27,21 @@ oph=$(parser $inputfile oph)
 ninterm=$(parser $configfile ninterm)
 ntot=$(parser $configfile ntot)
 
-#conversion into proper numbers
+#conversion into usable variables
 oph=$(printf "%.f" $oph)
 ninterm=$(printf "%.f" $ninterm)
 ntot=$(printf "%.f" $ntot)
-
+seed="${seedjob%.*}"               #seed is now identical to seejob except 
+                                   #that it does not contain the extension
 
 #computation
 IJON=$((24*$oph))                  #individual job output number, eg number of outputs per job
 NJ=$(($ntot/($IJON*$ninterm)))     #number of jobs recquiered by the user
 
 
+#main loop
+#--------------------------------------------------
 i=1
-seed="${seedjob%.*}" #seed is now identical to seejob except that it does not contain the extension
 
 
 #create and edit the new jobs
@@ -43,6 +53,7 @@ do
     cp $seedjob $newfile
     sed -i "s/\$EXECUTABLE/\$EXECUTABLE -s $restart/g" $newfile
     ## DANGER AHEAD
+    #//////////////////////////////////////////////////
     if [ $i -eq 1 ] 
     then
         oarsub -S $newfile
@@ -50,10 +61,11 @@ do
 
     if [ $i -gt 1 ]
     then
-        jobnumber=$(oarstat | grep crobert | tail -1 | cut -d ' ' -f 1)
-        oarsub -S -a $jobnumber $newfile
+        jobnumber=$(oarstat | grep $USER | tail -1 | cut -d ' ' -f 1) #get the number of the previousjob
+        oarsub -S -a $jobnumber $newfile                              #submit the newjob, as an appendage of the previous one
     fi
     #END OF DANGER ZONE
+    #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     i=$(($i+1))
 done
 cd -
