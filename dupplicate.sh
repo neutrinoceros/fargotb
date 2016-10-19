1d923366510034f46e79cf26b1be07583a9f76ea
Clément Robert <clement.robert@protonmail.com>
2016-10-19 11:19:15 +0200

    add dupplicate.sh

---
+++ dupplicate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This program intends to copy a base simulation with all its filetree 
+# but rejecting the outputs
+
+base=$1
+target=$2
+
+rsync -av --exclude='output/*' --exclude='OAR*' $base/ $target
+
+sed -i "s/$base/$target/g" $target/jobs/*oar
+sed -i "s/$base/$target/g" $target/input*/*par
+
+# security : we force the user to change persmissions before they can run the simulation in case there is still something wrong
+chmod -x $target/*exe 
