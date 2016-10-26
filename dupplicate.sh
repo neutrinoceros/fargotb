e3ec6e06b8c5bc83a8b032e84c79200270d744f4
Clément Robert <clement.robert@protonmail.com>
2016-10-26 14:58:37 +0200

    bug fix for dupplicate.sh : now it should work regardless of the fact that inputs are provided with end '/' or not

--- dupplicate.sh
+++ dupplicate.sh
@@ -1,17 +1,54 @@
 #!/bin/bash
 
-# This program intends to copy a base simulation with all its filetree 
-# but rejects the outputs
-#
-# known issue : automatic replacement of dir name in files may not work
-#               if the user provides $base ending with a "/" character
-base=$1
-target=$2
+# --------------------------
+# Author : Clément Robert
+# written october 2016
+# --------------------------
+# This program copies a base simulation with all its file-tree 
+# except the output files
+
+# DEFINITIONS
+#----------------------------------------------------------------------
+
+function tailcut {
+    #this function removes the last character of $1 if it matches $2
+    if [ ${1:(-1)} == $2 ]
+    then
+        res=${1:0:$((${#1}-1))}
+    else
+        res=$1
+    fi
+    echo "$res"    
+}
+
+
+# PARSING
+#----------------------------------------------------------------------
+
+base=$(tailcut $1 "/")
+target=$(tailcut $2 "/")
+
+echo $base
+echo $target
+
+
+# SYNCHRONIZATION
+#----------------------------------------------------------------------
 
 rsync -av --exclude='output/*' --exclude='OAR*' $base/ $target
+
+
+# AUTO-EDITION of files mentioning their own location
+#----------------------------------------------------------------------
+# /!\ This part may still be subject to bug corrections
 
 sed -i "s!$base!$target!g" $target/jobs/*oar
 sed -i "s!$base!$target!g" $target/input*/*par
 
-# security : we force the user to change persmissions before they can run the simulation in case there is still something wrong
+
+# SECURITY
+#----------------------------------------------------------------------
+# we force the user to change persmissions before they can 
+# run the simulation in case there is still something wrong
+
 chmod -x $target/*exe 
