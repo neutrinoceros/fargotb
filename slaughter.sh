afb9687d2d12bf2ea116c377e4cc802b28a7945c
Clément Robert <clement.robert@protonmail.com>
2016-10-26 15:45:03 +0200

    improved slaughter.sh : it now asks for confirmation

--- slaughter.sh
+++ slaughter.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
+#
+# --------------------------
+# Author : Clément Robert
+# written october 2016
+# --------------------------
+# This program politely asks if you really wish it 
+# to kill *all* your simulations, then does.
 
-# Careful : this program kills ALL your simulations without confirmation
 
-tokill=$(oarstat | grep $USER | cut -d ' ' -f 1)
-oardel $tokill
+# PARSING
+#----------------------------------------------------------------------
+
+jobs=$(oarstat | grep $USER)
+tokill=$(echo $jobs |cut -d ' ' -f 1)
+
+
+# PROCEDURE
+#----------------------------------------------------------------------
+
+echo "You are about to kill the following jobs :"
+echo -e "\n$jobs\n" 
+read -p "proceed (y/[n])?    " choice
+case "$choice" in
+    y|Y|yes|YES ) oardel $tokill;;
+    *) echo "I'm sorry Dave. I'm afraid I can't do that";;
+esac
