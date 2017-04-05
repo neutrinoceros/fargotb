1e3fe4ae880ae59879c4924034c4275873b534bd
neutrinoceros <clement.robert@protonmail.com>
2017-04-05 17:33:46 +0200

    improve prototyp

--- lib_parsing/calques.py
+++ lib_parsing/calques.py
@@ -26,34 +26,28 @@ def scaleHeight(r,h0,beta=0.) :
     return h0*r**(1.+beta)
 
 
-def getScaleHeightsWherePlanetsAre(planetcfgfile) :
-    
+def getScaleHeightsWherePlanetsAre(planetcfgfile,h0,betas) :
+    hs = scaleHeight(smas,h0,beta)
+    return hs
 
 # This is really what motivated this "calque" branch -----------------------------------
 
-def findWKZ(border) :
+def findWKZ(rmed,rmin,rmax,aspectratio,beta,border) :
     #WKZ stands for "wave killing zone"
     #border should be 0 or 1 to denote "in" and "out" respectively
 
-    PLANETS     = parseString(config, 'Planets'         )#wip, this is not right
-    SPACING     = parseString(config, 'RadialSpacing'   )
-    ASPECTRATIO = parseValue (config, 'AspectRatio'     )
-    BETA        = parseValue (config, 'FlaringIndex'    )
-    NRAD        = parseValue (config, 'nrad'            )
-    NSEC        = parseValue (config, 'nsec'            )
-    RMIN        = parseValue (config, 'rmin',      float)
-    RMAX        = parseValue (config, 'rmax',      float)
-
-    RMED = getrad(RMIN,RMAX,NRAD,DR,'d',SPACING)
-    RINF = getrad(RMIN,RMAX,NRAD,DR,'vr',SPACING)
-
     if border == 0 : #int
-        d1 = RMED[25]-RMED[0]
-        d2 = 5.0*scaleHeight(RMIN,ASPECTRATIO,BETA)
-        d3 = .15*(1.-Rinf)#this "1" stands for the current semi-major axis of the closest planet, should be refined in the general case
-    else : #ext
-        d1 = RMED[-1]-RMED[-25]
-        d2 = 5.0*scaleHeight(RMAX,ASPECTRATIO,BETA)
-        d3 = .15*(1.-Rinf)#this "1" stands for the current semi-major axis of the closest planet, should be refined in the general case
+        d1 = rmed[25]-rmed[0]
+        d2 = 5.0*scaleHeight(rmin,aspectratio,beta)
+        d3 = .15*(1.-rmin)#this "1" stands for the current semi-major axis of the closest planet, should be refined in the general case
+        w  = max([d1,d2,d3])#width of the wave killing zone
+        WKZrads = rmin, rmin+w
 
-    return max([d1,d2,d3]) #width of the wave killing zone
+    else : #ext
+        d1 = rmed[-1]-rmed[-25]
+        d2 = 5.0*scaleHeight(rmax,aspectratio,beta)
+        d3 = 0.
+        w  = max([d1,d2,d3])
+        WKZrads = rmax-w, rmax
+
+    return WKZrads
