be5215f1a7f13174312c04bd8046fbada33c172c
Clément Robert <clement.robert@protonmail.com>
2017-04-08 11:10:55 +0200

    add arguments to script

--- stitchup.py
+++ stitchup.py
@@ -8,14 +8,20 @@
 
 from lib_parsing import *
 
-#args = getScriptArgs()
-N = 20
+args = getScriptArgs()
+if len(args) != 3 :
+    print "error : wrong number of arguments"
+    exit
+
+N = int(args[0])
+innerdisk_path = args[1]
+outerdisk_path = args[2]
 
 for k,tag in TAGS.items() :
     try :
-        with open("../inner_ref/out/gas{0}{1}.dat".format(tag,N),'r') as fi :
+        with open("{0}/gas{1}{2}.dat".format(innerdisk_path,tag,N),'r') as fi :
             inner_lines = fi.readlines()
-        with open("../outer_ref/out/gas{0}{1}.dat".format(tag,N),'r') as fi :
+        with open("{0}/gas{1}{2}.dat".format(outerdisk_path,tag,N),'r') as fi :
             outer_lines = fi.readlines()
         lines = inner_lines + outer_lines
         with open("out/gas{0}{1}.dat".format(tag,N),'w') as fi :
