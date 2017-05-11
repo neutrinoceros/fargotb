b0e3fd62b8e040bad6005cd5e12a79bae24e59f7
Clément Robert <clement.robert@protonmail.com>
2017-05-11 15:24:54 +0200

    add rotated theta field

--- augmentedmap.py
+++ augmentedmap.py
@@ -15,9 +15,10 @@ import matplotlib.pyplot as plt
 #args = getScriptArgs()
 
 config = ("/home/crobert/Bureau/sandboxPLOT2D/data/in/phase0.par") #tmp
-bg_key = "l"#tmp
+bg_key = "d"#tmp
 NOUT = 20#tmp
-crop_limit = 3.#tmp
+crop_limit = 5.#tmp
+azim_crop = True
 
 
 
@@ -59,9 +60,7 @@ ax = fig.add_subplot(111,aspect='auto')
 bg_field, bgfile = get2Dfield(bg_key,NRAD,NSEC,OUTDIR,NOUT)
 bg_used_radii    = getrad(RMIN,RMAX,NRAD,DR,bg_key,SPACING)
 
-# crop plotting region, cut out fields (optional)  
-# by default, the planet should be centered (this may be hard, especially near angular boundaries)
-# dev note : croping should be done BEFORE plotting anything
+# crop plotting region, cut out fields (optional)
 def Hill_radius(r_p,q_p) :
     return r_p*(q_p/3)**(1./3)
 
@@ -76,6 +75,15 @@ def findRadialLimits(r_p,q_p,rads,croper=5.) :
         jmax -=1
     return jmin,jmax
 
+def findAzimLimits(thetas,croper=5.) :
+    R_H = Hill_radius(r_p,q_p)
+    ns = len(thetas)
+    jmin,jmax = 0,nr-1
+    while r_p*(0-thetas[imin]) < r_p-croper*R_H :
+        imin +=1
+    while r_p*thetas[imax] > r_p+croper*R_H :
+        imax -=1
+    return imin,imax
 
 def crop_field(field,jmin,jmax) :
     cfield = field[jmin:jmax,:]
@@ -88,25 +96,35 @@ def crop_field(field,jmin,jmax) :
 
 def rotate(field,thetas,theta_p) :
     ns = len(thetas)
-    j_p = 0
-    while thetas[j_p] < theta_p :
-        j_p += 1
+    i_p = 0
+    while thetas[i_p] < theta_p :
+        i_p += 1
 
-    cesure  = ns/2 - j_p
-    ffield1 = np.concatenate((field[:,-cesure:ns-1],field[:,0:j_p]),axis=1)
-    ffield2 = field[:,j_p:-cesure]
+    cesure  = ns/2 - i_p
+    ffield1 = np.concatenate((field[:,-cesure:ns-1],field[:,0:i_p]),axis=1)
+    ffield2 = field[:,i_p:-cesure]
     ffield  = np.concatenate((ffield1,ffield2),axis=1)
     return ffield,cesure
 
 
 Jmin,Jmax = findRadialLimits(r_p,q_p,bg_used_radii,crop_limit)
 bg_field,cesure = rotate(bg_field,base_theta,theta_p)
-bg_used_radii_crop = bg_used_radii[Jmin:Jmax]
 
+
+bg_used_radii_crop = bg_used_radii[Jmin:Jmax]
 bg_field_crop = bg_field[Jmin:Jmax,:]
 
+#These two lines need to be run after rotation...
+i_p = 0
+while base_theta[i_p] < theta_p :
+    i_p += 1
+j_p = 0
+while bg_used_radii_crop[j_p] < r_p :
+    j_p += 1
 
-
+new_thetas1   = base_theta[i_p:-cesure]
+new_thetas2   = base_theta[-cesure:NSEC-1]
+bg_used_theta = np.concatenate((new_thetas1,new_thetas2),axis=0)
 
 
 im = ax.imshow(bg_field_crop,cmap='viridis',aspect="auto")
@@ -124,12 +142,6 @@ ax.set_yticklabels(ytickslab)
 ax.set_xlabel(r"$\theta$", size=20)
 ax.set_ylabel(r"$r$",      size=20)
 
-#test zone
-#ax2.imshow(bg_field,cmap='viridis',aspect="auto")
-#ax2.set_ylim(0,NRAD)
-#ax2.set_xticks([0,NSEC/4,NSEC/2,3*NSEC/4,NSEC])
-#ax2.set_xticklabels([r"$-\pi$",r"$-\pi/2$",r"$0$",r"$\pi/2$",r"$\pi$"])
-
 # draw hill sphere(s) #todo : make this optional
 def circle(x0,y0,r,theta) :
     return x0+r*np.cos(theta), y0+r*np.sin(theta)
@@ -137,11 +149,8 @@ def circle(x0,y0,r,theta) :
 R_H = Hill_radius(r_p,q_p)
 thetas=np.linspace(0,2*np.pi,100)
 
-j_p = 0
-while bg_used_radii_crop[j_p] < r_p :
-    j_p+=1
 
-ax.plot( *circle(NSEC/2,j_p,R_H/(r_p*dtheta),thetas),     c='k', ls='--')
+ax.plot( *circle(NSEC/2,j_p,R_H/(r_p*dtheta),thetas),         c='w', ls='--')
 ax.plot( *circle(NSEC/2,j_p,0.3*R_H/(r_p*dtheta),thetas),     c='r', ls='-')
 
 cb = fig.colorbar(im)
