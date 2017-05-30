0d1bc1d1a0d84938f129f91890e051b58b9479bc
Clément Robert <clement.robert@protonmail.com>
2017-05-30 22:24:51 +0200

    bugfix omegaframe

--- lib_parsing/physics_functions.py
+++ lib_parsing/physics_functions.py
@@ -4,7 +4,8 @@ def Hill_radius(r_p,q_p) :
     return r_p*(q_p/3)**(1./3)
 
 def OmegaFrame(r_p,q_p) :
-    return np.sqrt((1.+q_p)/r_p**3)#todo : check
+    a_p = r_p #tmp
+    return a_p*np.sqrt((1.+q_p)/r_p**3)#todo : check
 
 def atan2(x,y) :
     if x > 0. :
